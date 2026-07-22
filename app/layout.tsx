03cc041e4548001ab2151ccb51b875fe5e62717d
Mateusz Gontarek <mateuszgontarek2020@gmail.com>
2026-07-22 21:34:21 +0200

    zmiana tytułu

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import Providers from "@/components/Providers";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Innovations Hub Foundation - platforma",
+  title: "Innovations Hub Foundation",
   description: "Platforma matchmakingowa Innovations Hub Foundation umożliwia uczestnikom programu inkubacyjnego utworzenie profilu, wypełnienie ankiety dotyczącej kompetencji i zainteresowań oraz znalezienie współzałożycieli i członków zespołów do realizacji projektów startupowych. Logowanie przez Google służy do bezpiecznej identyfikacji użytkownika i dostępu do platformy.",
 };
 
